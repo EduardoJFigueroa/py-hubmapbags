98d313750baaa2ccb14c2177075d7f9f9a6217f2
Ivan Cao-Berg <icaoberg@gmail.com>
2023-12-15 03:44:56 -0500

    Refactor `__get_dbgap_study_id` function. Fix logical OR operator from `|` to `or`. Remove unnecessary print statement.

--- hubmapbags/file.py
+++ hubmapbags/file.py
@@ -177,8 +177,8 @@ def __get_dbgap_study_id(file: str, dbgap_study_id: str) -> str:
     if dbgap_study_id == "" or dbgap_study_id is None:
         return ""
     else:
-        print(f'{file} : {str(file).find("tar.gz") > 0 | str(file).find("fastq.gz") > 0} {dbgap_study_id}')
-        if str(file).find("tar.gz") > 0 | str(file).find("fastq.gz") > 0:
+        print(f'{file} : {str(file).find("tar.gz") > 0 or str(file).find("fastq.gz") > 0} {dbgap_study_id}')
+        if str(file).find("tar.gz") > 0 or str(file).find("fastq.gz") > 0:
             return dbgap_study_id
         else:
             return ""
